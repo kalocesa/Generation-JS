aed42a375ad7b81544380cf4e579ab14d528bbd1
kalocesa <kalocesa95@gmail.com>
2025-02-04 10:27:29 -0600

    add: new file expressions and operators

--- js-04-arrays_and_loops/src/js/main.js
+++ js-04-arrays_and_loops/src/js/main.js
@@ -149,9 +149,141 @@ for (;;) {
 }
 
 // ------------------- Uso de break y label en ciclos anidados ----------------------------
+/*multiplicando;
 for (let i = 1; i <= 7; i++) {
-  for (let j = 1; j <= 10; j++) {
+  multiplicador: for (let j = 1; j <= 10; j++) {
     console.log(`${i} x ${j} = ${i * j}`);
-    if (i >= 4) break;
+
+    if (i >= 4) breakmultiplicando;
   }
+}*/
+
+// ============ Ejercicio mental =============================
+let myIteration;
+for (myIteration = 0; myIteration <= 5; myIteration++) {
+  console.log("For loop", myIteration); //  0...4
+  if (myIteration === 3) break;
 }
+console.log("Final", myIteration); // 5
+
+// ================ Reassigment and Redeclaration========
+/*
+ Declarar una variable significa que se reserva el nombre 
+ en memoria en el scope actual. 
+ 
+ Inicializar una variable es establecer el valor de la variable
+*/
+// Pueden ser reasignadas y redeclaradas dentro del
+// mismo contexto de función.
+var userName = 10;
+userName = 20; // Reasignación válida
+var userName = 30; // Redeclaración válida en el mismo contexto de función
+// Pueden ser reasignadas dentro del mismo bloque y no pueden
+// ser redeclaradas en el mismo ámbito.
+let age = 10;
+age = 20; // Reasignación válida
+// let age = 30; // Error: No se puede redeclarar en el mismo ámbito
+// No pueden ser reasignadas después de su inicialización y tampoco
+// pueden ser redeclaradas en el mismo ámbito.
+const address = 10;
+// address = 20; // Error: No se puede reasignar
+// const address = 30; // Error: No se puede redeclarar
+
+// ================ Function Scope ====================
+/*
+  Variables declaradas con var, let y const tienen un alcance de función. 
+  Esto significa que están limitadas al contexto de la función 
+  en la que se declaran.
+*/
+function functionScope() {
+  var myVar = 10;
+  let myLet = 20;
+  const myConst = 30;
+  console.log(myVar);
+  console.log(myLet);
+  console.log(myConst);
+}
+functionScope();
+// console.log(myVar);
+// console.log(myLet);
+// console.log(myConst);
+
+//Block Scope ====================
+/*
+  Variables declaradas con let y const tienen un alcance de bloque. 
+  Esto significa que están limitadas al bloque en el que se declaran.
+*/
+{
+  var a = "a";
+  let b = "b";
+  const c = "c";
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+// ================ Hoisting =======================
+/*
+  El "hoisting" en JavaScript es un comportamiento que ocurre durante 
+  la fase de compilación antes de la ejecución del código. 
+  Se refiere a la elevación (movimiento) de las declaraciones 
+  de variables y funciones al inicio de su contexto de ejecución
+*/
+/*
+  Las variables declaradas con var son elevadas al inicio de su contexto 
+  de ejecución (ya sea el ámbito de función o el ámbito global).
+  
+  Sin embargo, solo la declaración de la variable es elevada, 
+  no la asignación. Esto significa que la variable existe, 
+  pero su valor es undefined hasta que llega a la línea de asignación.
+*/
+console.log(phone); // undefined
+var phone = "55-65-123-123";
+console.log(phone); // 55-65-123-123
+
+/*
+  Las variables declaradas con let y const también se elevan al 
+  inicio de su contexto, pero a diferencia de var, no se inicializan 
+  automáticamente con undefined. La fase de inicialización de estas 
+  variables no ocurre hasta llegar a la línea de código donde se declaran.
+*/
+
+// ============== Ciclo While ======================
+//                (while loop)
+/*
+ Crea un bucle que ejecuta una sentencia mientras la condición especificada
+ se cumpla.
+ Sintaxis:
+    while( condición) sentencia;
+    while ( condición ){
+        sentencias;
+    }
+*/
+
+// ============== Ciclo While ======================
+//                (while loop)
+/*
+ Crea un bucle que ejecuta una sentencia mientras la condición especificada
+ se cumpla.
+ Sintaxis:
+    while( condición) sentencia;
+    while ( condición ){
+        sentencias;
+    }
+*/
+/*while (confirm("¿Quieres que te genere tu número de la suerte ")) {
+  // ramdom devuelve un número aleatorio entre 0(incluido) y 1(excluido)
+  console.log(`Tu número de la suerte es ${Math.random()} `);
+  // Número aleatorio del 0 al 10(excluido)
+  console.log(`Tu número de la suerte es ${Math.random() * 10} `);
+  // Número aleatorio del 1 al 11(excluido)           1 - 10.999999
+  console.log(`Tu número de la suerte es ${Math.random() * 10 + 1} `);
+  // Número aleatorio entero del 1 al 10(incluido)
+  alert(`Tu número de la suerte es ${Math.floor(Math.random() * 10 + 1)} `);
+}*/
+
+// Generar 100 números aleatorios en el rango de 1 al 50(incluyendo).
+// Ordenar e imprimir de manera descendente
